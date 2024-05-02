2f0ee0dca2b130ed5bf2491c77aa20bca9079ceb
0x0FACED <https.genius@gmail.com>
2024-05-02 18:40:05 +0300

    Added fields

--- internal/models/chat.go
+++ internal/models/chat.go
@@ -1,4 +1,7 @@
 package models
 
 type Chat struct {
+	ID         int `json:"id" db:"id"`
+	FirstUser  int `json:"first_user" db:"first_user"`
+	SecondUser int `json:"second_user" db:"second_user"`
 }
