42c7f2688b9651dc4318caef39deb4bee0a68127
0x0FACED <https.genius@gmail.com>
2024-05-02 18:49:31 +0300

    Added fields and hash password

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,4 +1,21 @@
 package models
 
+import (
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
 type User struct {
+	ID               int       `json:"id" db:"id"`
+	Name             string    `json:"name" db:"name"`
+	Username         string    `json:"username" db:"username"`
+	Password         string    `json:"password" db:"password"`
+	RegistrationDate time.Time `json:"registration_date" db:"registration_date"`
+	Description      string    `json:"description" db:"description"`
+}
+
+func (u *User) HashPassword(pass string) error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	u.Password = string(hashed)
+	return err
 }
