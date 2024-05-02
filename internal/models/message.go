254f0bed8fd8ebaa146a56deadd4d93b19376272
0x0FACED <https.genius@gmail.com>
2024-05-02 18:47:20 +0300

    Added fields

--- internal/models/message.go
+++ internal/models/message.go
@@ -1,4 +1,11 @@
 package models
 
+import "time"
+
 type Message struct {
+	ID         int       `json:"id" db:"id"`
+	SenderID   int       `json:"sender_id" db:"sender_id"`
+	ReceiverID int       `json:"receiver_id" db:"receiver_id"`
+	Text       string    `json:"text" db:"text"`
+	Timestamp  time.Time `json:"timestamp" db:"timestamp"`
 }
