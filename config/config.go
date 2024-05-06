758034597aa57af6a738a9ebc2c7e55e32c716fc
0x0FACED <https.genius@gmail.com>
2024-05-06 15:38:45 +0300

    Added `RedisConfig` struct

--- config/config.go
+++ config/config.go
@@ -1,5 +1,7 @@
 package config
 
+import "time"
+
 type ServerConfig struct {
 	Host string `toml:"host"`
 	Port int    `toml:"port"`
@@ -14,7 +16,18 @@ type DatabaseConfig struct {
 	DriverName string `toml:"driver"`
 }
 
+type RedisConfig struct {
+	Host        string        `toml:"host"`
+	Port        int           `toml:"port"`
+	Network     string        `toml:"network"`
+	Username    string        `toml:"username"`
+	Password    string        `toml:"password"`
+	DialTimeout time.Duration `toml:"dial_timeout"`
+	MaxTries    int           `toml:"max_tries"`
+}
+
 type Config struct {
 	Server   ServerConfig   `toml:"server"`
 	Database DatabaseConfig `toml:"database"`
+	Redis    RedisConfig    `toml:"redis"`
 }
