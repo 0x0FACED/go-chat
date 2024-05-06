260be5395684bef0a39c92d3994454ad73b99f76
0x0FACED <https.genius@gmail.com>
2024-05-06 15:39:39 +0300

    Added some constants

--- internal/utils/const.go
+++ internal/utils/const.go
@@ -1,12 +1,29 @@
 package utils
 
 const (
+	ErrOpenDB  = "cant open db:"
+	ErrPingDB  = "cant ping db:"
+	ErrCloseDB = "cant close db:"
+)
+
+const (
 	ErrUsernameLength = "username len must be <= 16 and not 0"
 	ErrPasswordLength = "pass len must be >= 5"
 
-	ErrIncorrectUsernameOrPass = "incorrect username or pass"
-	ErrRollbackTx              = "cant rollback tx:"
-	ErrCommitTx                = "cant commit tx:"
+	ErrUserExists = "cant register, user exists"
 
-	QueryLoginTx = "SELECT id, name from users where username = ? and password = ?"
+	ErrIncorrectUsernameOrPass = "incorrect username or pass"
+)
+
+const (
+	ErrBeginTx     = "cant begin tx:"
+	ErrRollbackTx  = "cant rollback tx:"
+	ErrCommitTx    = "cant commit tx:"
+	ErrPrepareTx   = "cant prepare tx:"
+	ErrExecQueryTx = "cant exec or query to db tx:"
+)
+
+const (
+	QueryLoginTx    = "SELECT * from users where username = ? and password = ?"
+	QueryRegisterTx = "INSERT INTO users (name, username, password, description) VALUES ($1, $2, $3, $4)"
 )
