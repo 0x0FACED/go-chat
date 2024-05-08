590458e005d59d0492a4fd8d09b65b0194abf90b
0x0FACED <https.genius@gmail.com>
2024-05-08 16:02:16 +0300

    Moved prepareRoutes

--- internal/http/handlers.go
+++ internal/http/handlers.go
@@ -1,6 +1,16 @@
 package http
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (s *Server) prepareRoutes() {
+	s.r.Handle(http.MethodPost, "/register", s.handleRegister)
+	s.r.Handle(http.MethodPost, "/login", s.handleLogin)
+	s.r.Handle(http.MethodPost, "/send_message", s.handleSendMessage)
+	s.r.Handle(http.MethodGet, "/get_messages", s.handleGetMessages)
+}
 
 func (s *Server) handleRegister(ctx *gin.Context) {
 	// ...
