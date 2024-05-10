5158f82199edf2088cebbf61a6a07b0bb2d7db0d
0x0FACED <https.genius@gmail.com>
2024-05-10 16:00:08 +0300

    edited: `HashPassword()`, created: `CompareHashPassword()`, edited json tags

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,9 @@
 package models
 
 import (
-	"golang.org/x/crypto/bcrypt"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -10,12 +11,22 @@ type User struct {
 	Name             string    `json:"name" db:"name"`
 	Username         string    `json:"username" db:"username"`
 	Password         string    `json:"password" db:"password"`
-	RegistrationDate time.Time `json:"registration_date" db:"registration_date"`
-	Description      string    `json:"description" db:"description"`
+	RegistrationDate time.Time `json:"reg_date" db:"registration_date"`
+	Description      string    `json:"desc" db:"description"`
 }
 
-func (u *User) HashPassword(pass string) error {
+func (u *User) HashPassword(pass string) (string, error) {
 	hashed, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
-	u.Password = string(hashed)
+	if err != nil {
+		return pass, err
+	}
+	return string(hashed), nil
+}
+
+func (u *User) CompareHashPassword(pass string, hashed string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(pass))
+	if err != nil {
 	return err
+	}
+	return nil
 }
