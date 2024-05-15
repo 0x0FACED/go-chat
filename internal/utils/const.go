f0c8f3175f18ce066fee04ebd1a945800c6fd64c
0x0FACED <https.genius@gmail.com>
2024-05-15 16:37:17 +0300

    refactored: string literals for queries, added: chreate caht and find chat queries

--- internal/utils/const.go
+++ internal/utils/const.go
@@ -25,8 +25,10 @@ const (
 )
 
 const (
-	QueryLoginTx       = "SELECT id, name, username, registration_date, description FROM users WHERE username = $1 AND password = $2"
-	QueryRegisterTx    = "INSERT INTO users (name, username, password, description) VALUES ($1, $2, $3, $4)"
-	QueryGetPasswordTx = "SELECT password FROM users WHERE username = $1"
-	QuerySaveMessageTx = "INSERT INTO messages (sender_id, receiver_id, text, chat_id) VALUES ($1, $2, $3, $4) RETURNING id"
+	QueryLoginTx       = `SELECT id, name, username, registration_date, description FROM users WHERE username = $1 AND password = $2`
+	QueryRegisterTx    = `INSERT INTO users (name, username, password, description) VALUES ($1, $2, $3, $4)`
+	QueryGetPasswordTx = `SELECT password FROM users WHERE username = $1`
+	QuerySaveMessageTx = `INSERT INTO messages (sender_id, receiver_id, text, chat_id) VALUES ($1, $2, $3, $4) RETURNING id, timestamp`
+	QueryCreateChatTx  = `INSERT INTO chats (first_user_id, second_user_id) VALUES ($1, $2) RETURNING id`
+	QueryFindChatTx    = `SELECT id FROM chats WHERE (first_user_id = $1 AND second_user_id = $2) OR (first_user_id = $2 AND second_user_id = $1)`
 )
